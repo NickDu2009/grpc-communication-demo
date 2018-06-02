6b483401242eb2c4cbcb7783bac62baf01cef0ed
NickDu2009 <nickdu2009@gmail.com>
2018-06-02 08:15:04 +0800

    role-microservice

---
+++ roles-microservice/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"errors"
+	pb "github.com/NickDu2009/grpc-communication-demo/roles-microservice/pb"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"net"
+	"log"
+)
+
+type Server struct {
+	userRoles map[int32][]*pb.Role
+	roles     []*pb.Role
+}
+
+func (s *Server) GetRoles(_ context.Context, _ *pb.EmptyRequest) (*pb.RolesReply, error) {
+	return &pb.RolesReply{
+		Roles: s.roles,
+	}, nil
+}
+
+func (s *Server) GetUserRole(_ context.Context, req *pb.GetUserRoleRequest) (*pb.UserRoleReply, error) {
+	roles, ok := s.userRoles[req.UserId]
+	if !ok {
+		return nil, errors.New("user not found")
+	}
+
+	return &pb.UserRoleReply{
+		UserId: req.UserId,
+		Roles:  roles,
+	}, nil
+}
+
+func main() {
+	var (
+		normal = &pb.Role{
+			Id:   1,
+			Name: "normal",
+		}
+
+		editor = &pb.Role{
+			Id:   2,
+			Name: "editor",
+		}
+
+		admin = &pb.Role{
+			Id:   3,
+			Name: "admin",
+		}
+
+		superUser = &pb.Role{
+			Id:   4,
+			Name: "super user",
+		}
+	)
+
+	lis, err := net.Listen("tcp", "localhost:6000")
+	if err != nil {
+		log.Fatalf("failed to initializa TCP listen: %v", err)
+		}
+		defer lis.Close()
+
+	server := grpc.NewServer()
+	roleServer := &Server{
+		userRoles:map[int32][]*pb.Role{
+			1:{normal},
+			2:{normal, editor},
+			3:{normal},
+			4:{normal,editor, admin},
+			5:{normal, editor, admin, superUser},
+		},
+		roles:[]*pb.Role{normal, editor, admin, superUser},
+	}
+
+	pb.RegisterRolesServer(server, roleServer)
+
+	server.Serve(lis)
+}
